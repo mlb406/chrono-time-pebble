a8a0879a5b9c57814586843702cb0ed91598ee48
Max Beckett <mlb406@gmail.com>
2016-01-07 22:03:11 +0000

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,41 @@
+#include <pebble.h>
+
+static Window *window;
+static Layer *bg_layer;
+
+static void bg_draw_proc(Layer *layer, GContext *ctx) {
+	graphics_context_set_fill_color(ctx, GColorBlack);
+}
+
+static void main_window_load() {
+	bg_layer = layer_create(GRect(0, 0, 144, 168));
+	layer_set_update_proc(bg_layer, bg_draw_proc);
+	layer_add_child(window_get_root_layer(window), bg_layer);
+}
+
+static void main_window_unload() {
+	layer_destroy(bg_layer);
+}
+
+static void init() {
+	window = window_create();
+
+	window_set_background_color(window, GColorDarkGray);
+
+	window_set_window_handlers(window, (WindowHandlers) {
+		.load = main_window_load,
+		.unload = main_window_unload,
+	});
+
+	window_stack_push(window, true);
+}
+
+static void deinit() {
+	window_destroy(window);
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
+}
